b2e267924d09df88d95b4bb5fe0e9501857b3ded
yashkumarsaurabh <65075425+yashkumarsaurabh@users.noreply.github.com>
2026-01-04 02:26:20 +0530

    form with signal without error message and validators

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,38 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { form, Field } from '@angular/forms/signals';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
+
+interface LoginForm {
+  email: string,
+  password: string,
+  address: string
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatIconModule, Field, MatFormFieldModule],
+
 })
 export class App {
   protected readonly title = signal('angular-signal-form');
+
+  loginFormModel = signal<LoginForm>({
+    email: '',
+    password: '',
+    address: ''
+  });
+  
+  loginForm = form(this.loginFormModel);
+
+  onSubmit(e: Event) {
+    e.preventDefault();
+
+    const formValue = this.loginFormModel();
+  }
 }
