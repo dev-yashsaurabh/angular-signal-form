e43c9be3bc7d9051d7c0d6eef4958b5e5a77902e
yashkumarsaurabh <65075425+yashkumarsaurabh@users.noreply.github.com>
2026-01-07 21:47:31 +0530

    changes for validators

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { form, Field, required, email, min, minLength } from '@angular/forms/signals';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { CommonModule } from '@angular/common';
@@ -27,13 +27,13 @@ export class App {
     password: '',
     address: ''
   });
-  
+
   loginForm = form(this.loginFormModel, (schema) => {
-    required(schema.email, {message: "Email is required"});
-    email(schema.email, {message: "Email is not valid!"});
-    minLength(schema.password, 6, {message: "Minimum length should be 6!"});
-    required(schema.password, {message: "Password is required"});
-    required(schema.address, {message: "Address is required!"});
+    required(schema.email, { message: "Email is required" });
+    email(schema.email, { message: "Email is not valid!" });
+    minLength(schema.password, 6, { message: "Minimum length should be 6!" });
+    required(schema.password, { message: "Password is required" });
+    required(schema.address, { message: "Address is required!" });
   });
 
   onSubmit(e: Event) {
